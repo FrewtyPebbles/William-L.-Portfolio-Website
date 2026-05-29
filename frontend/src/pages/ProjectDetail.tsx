fb1e5f03528c076a8b425a55544d19da8f322d99
William L. <frewtypebbles@gmail.com>
2026-05-28 19:44:04 -0700

    Made login with google google branding compliant.

--- frontend/src/pages/ProjectDetail.tsx
+++ frontend/src/pages/ProjectDetail.tsx
@@ -8,7 +8,7 @@ import ProgressIndicator, { ProjectProgress } from '@/components/progress-indica
 import { markdownComponents } from '@/lib/markdown-components';
 import remarkGfm from 'remark-gfm'
 import FetchingProjectPageSkeleton from '@/components/fetching-project-page-skeleton';
-import { get_project_url, postComment } from '@/lib/utils';
+import { get_project_url, postComment, usePrefersDark } from '@/lib/utils';
 import { ProjectConfig } from '@/types/project';
 import ImageCarousel from '@/components/image-carousell';
 import { useComments } from '@/lib/comments-context';
@@ -98,10 +98,15 @@ function CommentsSection({project, slug}:CommentsSectionProps) {
     fetch_comments(null)
   }, [])
 
+  let login_with_google_link = `/api/login?return_uri=${encodeURIComponent(window.location.pathname)}`
+
   let post_comment_section = (
-    <a href={`/api/login?return_uri=${encodeURIComponent(window.location.pathname)}`}>
-      login with google to comment
-    </a>
+    <div className='flex gap-4 p-2'>
+      <SignInWithGoogle href={login_with_google_link}/>
+      <span className='align-middle flex flex-col justify-center'>
+        to comment.
+      </span>
+    </div>
   );
   if (!loading_user && user) {
     
@@ -110,7 +115,7 @@ function CommentsSection({project, slug}:CommentsSectionProps) {
         <div className='flex'>
             <div>
                 <div className='flex justify-center pt-2'>
-                    <img src={user.avatar} alt="" className='h-10 w-10 rounded-full'/>
+                    <img onClick={(e) => window.location = login_with_google_link as string & Location} src={user.avatar} alt="" className='h-10 w-10 rounded-full'/>
                 </div>
                 <div className='flex flex-col justify-center p-1'>
                     {user.name}
@@ -176,5 +181,26 @@ function CommentsSection({project, slug}:CommentsSectionProps) {
     
     {comments_section}
   </div>
+  )
+}
+
+function SignInWithGoogle({className, href}:{className?:string, href:string}) {
+  return (
+    <button onClick={(e) => window.location = href as string & Location} className="gsi-material-button">
+      <div className="gsi-material-button-state"></div>
+      <div className="gsi-material-button-content-wrapper">
+        <div className="gsi-material-button-icon">
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlnsXlink="http://www.w3.org/1999/xlink" style={{display: "block"}}>
+            <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+            <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+            <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+            <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+            <path fill="none" d="M0 0h48v48H0z"></path>
+          </svg>
+        </div>
+        <span className="gsi-material-button-contents">Sign in with Google</span>
+        <span style={{display: "none"}}>Sign in with Google</span>
+      </div>
+    </button>
   )
 }
